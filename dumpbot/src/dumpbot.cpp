acf1b27dccc5d7ff7f390e9e7f80c8c983ee12e5
Playfish <1271087623@qq.com>
2018-05-11 14:54:34 +0800

    Update dumpbot.cpp

--- dumpbot/src/dumpbot.cpp
+++ dumpbot/src/dumpbot.cpp
@@ -33,17 +33,12 @@
 #include <std_msgs/Header.h>
 #include <stdlib.h>
 #include <dumpbot_msgs/SensorData.h>
-
+#include <move_base_msgs/MoveBaseAction.h>  
+#include <actionlib/client/simple_action_client.h> 
+using namespace std;
 namespace dumpbot
 {
 
-/**
- * @ brief Keeps msgs from navigation and send goal for taking out trash
- *
- * This Dumpbot will subscribe msgs from navigation and send goal where 
- * should taking out trash, also concert with command fo judge whether get object.
- *
- */
 class Dumpbot : public nodelet::Nodelet
 {
 public:
@@ -58,12 +53,6 @@ private:
   double goal_z_; /**< The position z of goal where dumpbot dumping garbage. >*/
   double orien_z_; /**< The orientation z of goal where dumpbot dumping garbage. >*/
 
-
-  /*!
-   * @brief OnInit method from node handle.
-   * OnInit method from node handle. Sets up the parameters
-   * and topics.
-   */
   virtual void onInit()
   {
     ros::NodeHandle& nh = getNodeHandle();
@@ -91,41 +80,65 @@ private:
     }
     return n;
   }
-  /*!
-   * @brief Callback for Dumpbot's MCU data.
-   * Callback for Dumpbot's MCU data. It finds whether
-   * go take out trash or not.
-   * @param MCU data message.
-   */
+
   void changeSensorDataCb(const dumpbot_msgs::SensorData::ConstPtr& data){
 
-    ROS_INFO_STREAM("Yes! I heard:[ ult_01: " <<data->ult_01<<" ult_02: " <<data->ult_02<<" ult_03: " <<data->ult_03<<" ult_01: " <<data->ult_04<<" ].");
-		
-	// if data right then send goal
-    geometry_msgs::PoseStamped next_goal;
-    next_goal.header.frame_id = "/map";
-    next_goal.pose.position.x = goal_x_;
-    next_goal.pose.position.y = goal_y_;
-    next_goal.pose.position.z = goal_z_;
-    next_goal.pose.orientation.x = 0.0;
-    next_goal.pose.orientation.y = 0.0;
-    next_goal.pose.orientation.z = orien_z_;
-    next_goal.pose.orientation.w = 1.0;
-    goalpub_.publish(next_goal);
+
+    cout << "I have received!!!!!!!!!!!!!!!!" << endl;
+    ROS_INFO_STREAM("Yes! I heard:[ ult_01: " <<data->ult_01<<" ult_02: " <<data->ult_02<<" ult_03: " <<data->ult_03<<" ult_04: " <<data->ult_04<<" ].");
+			cout<<"go "<<endl;
+         if((data->ult_01)<18000)
+    {
+  
+	    geometry_msgs::PoseStamped next_goal;
+	    next_goal.header.frame_id = "map";
+	    next_goal.pose.position.x = 0.0575;
+	    next_goal.pose.position.y = 0.194;
+	    next_goal.pose.position.z = goal_z_;
+	    next_goal.pose.orientation.x = 0.0;
+	    next_goal.pose.orientation.y = 0.0;
+	    next_goal.pose.orientation.z = 0.318;
+	    next_goal.pose.orientation.w = 0.95;
+	    goalpub_.publish(next_goal);
   }
-  /*!
-   * @brief Callback for Dumpbot's MCU data.
-   * Callback for Dumpbot's MCU data. It finds whether
-   * go take out trash or not.
-   * @param MCU data message.
-   */
+  if((data->ult_01)>18000)
+  {
+        if((data->ult_02)==1)
+      {
+        geometry_msgs::PoseStamped next_goal;
+       next_goal.pose.position.x = 2.18;
+      next_goal.pose.position.y = 1.88;
+      next_goal.pose.position.z = 0.0;
+      next_goal.pose.orientation.x = 0.0;
+      next_goal.pose.orientation.y = 0.0;
+      next_goal.pose.orientation.z = 0.008;
+      next_goal.pose.orientation.w = 1.0;
+      goalpub_.publish(next_goal);
+      }
+     if((data->ult_02)==2)
+      {
+           geometry_msgs::PoseStamped next_goal;
+	    next_goal.header.frame_id = "map";
+	    next_goal.pose.position.x = 0.0575;
+	    next_goal.pose.position.y = 0.194;
+	    next_goal.pose.position.z = goal_z_;
+	    next_goal.pose.orientation.x = 0.0;
+	    next_goal.pose.orientation.y = 0.0;
+	    next_goal.pose.orientation.z = 0.318;
+	    next_goal.pose.orientation.w = 0.95;
+	    goalpub_.publish(next_goal);
+      }
+     
+  }
+	
+}
+
   void changeDataCb(const std_msgs::String::ConstPtr& data){
     std::string determineData= data->data.c_str();
     char buff[1024];
 //	ROS_INFO("Yes! I heard:[%s]",determineData.c_str());
-    std::cout << hexstring2char(determineData, buff);  //buff中输出结果
+    std::cout << hexstring2char(determineData, buff);
     ROS_INFO_STREAM("Yes! I heard:[" <<buff<<" ].");
-	// load and parse data into int 
 
   }
     
@@ -137,5 +150,5 @@ private:
 };
 
 PLUGINLIB_DECLARE_CLASS(dumpbot, Dumpbot, dumpbot::Dumpbot, nodelet::Nodelet);
-} // namespace dumbot
+}
 
